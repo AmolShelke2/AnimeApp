5720520c55ba7ffa0488981dfbf5434529f714c7
Amol shelke <shelkeamol897@gmail.com>
2022-02-12 14:36:04 +0530

    Refactor code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,14 @@
-const getBTN = document.getElementById('btn')
-const animeBox = document.querySelector('.anime-box')
-const anime = document.querySelector('.anime')
-const animeName = document.querySelector('.hero-name')
+const getBTN = document.getElementById("btn");
+const animeBox = document.querySelector(".anime-box");
+const anime = document.querySelector(".anime");
+const animeName = document.querySelector(".hero-name");
 
-const api_url = `https://api.catboys.com/img`
+const api_url = `https://api.catboys.com/img`;
 
-getBTN.addEventListener('click', async function () {
-    const response = await fetch(api_url)
-    const data = await response.json()
-    anime.src = data.url
-    animeBox.style.display = 'block'
-    animeName.textContent = data.artist;
-})
+getBTN.addEventListener("click", async function () {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  anime.src = data.url;
+  animeBox.style.display = "block";
+  animeName.textContent = data.artist;
+});
